47db56161fbe509c6fefe411beca96c09b2e07ba
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2024-04-15 09:20:59 +0100

    chore: adjusted DB, tests still passing

--- backend/src/database.py
+++ backend/src/database.py
@@ -2,13 +2,11 @@ import sqlite3
 from flask import g, current_app
 
 def get_db():
-    if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
-    return g.db
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(current_app.config['DATABASE'])
+        db.row_factory = sqlite3.Row
+    return db
 
 def close_db(e=None):
     db = g.pop('db', None)
